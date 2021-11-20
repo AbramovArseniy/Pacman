486c77ec4e3903b8fa4443917d2f6603bc86867d
Замалаев Дмитрий <dzamalaev@gmail.com>
2021-11-20 14:55:31 +0300

    added animation pacman

--- pacman.py
+++ pacman.py
@@ -6,10 +6,11 @@ size = (600, 700)
 
 class Pacman(pygame.sprite.Sprite):
     direction: int  # направление 0 - стоит, 1 - влево, 2 - вправо, 3 - вверх, 4 - вниз
+    ind_image: str
 
     def __init__(self, x, y, screen=None, direction=0):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("sprites/pacman_left.png")
+        self.image = pygame.image.load("sprites/pacman_left2.png")
         self.image = pygame.transform.scale(self.image, (25, 25))
         self.game_over = False
         self.rect = self.image.get_rect()
@@ -19,16 +20,40 @@ class Pacman(pygame.sprite.Sprite):
         self.score = 0
         self.screen = screen
         self.rotate = 0
+        self.ind_image = "1"
+        self.ind_time = 1
 
     def move(self):
         if self.direction == 1:
-            self.rect.x -= 1
+            self.rect.x -= 2
+            self.change_image("left" + self.ind_image)
         elif self.direction == 2:
-            self.rect.x += 1
+            self.rect.x += 2
+            self.change_image("right" + self.ind_image)
         elif self.direction == 3:
-            self.rect.y -= 1
+            self.rect.y -= 2
+            self.change_image("up" + self.ind_image)
         elif self.direction == 4:
-            self.rect.y += 1
+            self.rect.y += 2
+            self.change_image("down" + self.ind_image)
+
+        if self.ind_time == 15:
+            if self.ind_image == "1":
+                self.ind_image = "2"
+            else:
+                self.ind_image = "1"
+            self.ind_time = 1
+
+        if self.rect.x <= 0:
+            self.rect.x = self.screen.get_width() - 25
+        elif self.rect.x + 25 >= self.screen.get_width():
+            self.rect.x = 0
+        elif self.rect.y <= 0:
+            self.rect.y = self.screen.get_height() - 25
+        elif self.rect.y + 25 >= self.screen.get_height():
+            self.rect.y = 0
+
+        self.ind_time += 1
         time.sleep(0.01)
 
     def draw(self):
@@ -48,7 +73,7 @@ class Pacman(pygame.sprite.Sprite):
     #                self.direction = 0
 
     def change_image(self, name_image):
-        self.image = pygame.image.load(name_image)
+        self.image = pygame.image.load(f"sprites/pacman_{name_image}.png")
         self.image = pygame.transform.scale(self.image, (25, 25))
 
 
@@ -61,20 +86,17 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
                     pacman.direction = 1
-                    pacman.change_image("sprites/pacman_left.png")
                 elif event.key == pygame.K_d:
                     pacman.direction = 2
-                    pacman.change_image("sprites/pacman_right.png")
                 elif event.key == pygame.K_w:
                     pacman.direction = 3
-                    pacman.change_image("sprites/pacman_up.png")
                 elif event.key == pygame.K_s:
                     pacman.direction = 4
-                    pacman.change_image("sprites/pacman_down.png")
-
         pacman.draw()
         pygame.display.flip()
         screen.fill("Black")
+    if pacman.game_over:
+        pacman.change_image("")
 
 
 if __name__ == "__main__":
