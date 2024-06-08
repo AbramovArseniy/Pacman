3291883d21017ed72db2745903b97a39240f3074
Denver31 <daniil.igoshev05@gmail.com>
2024-06-07 21:00:16 -0300

    чет изменил хз

--- func.py
+++ func.py
@@ -2,10 +2,11 @@ import pygame
 
 
 def update_screen(objects, screen):
+    screen.fill("Black")
+
     objects[0].change_direction(screen)
     objects[0].draw(objects)
     for ind, obj in enumerate(objects):
         if ind != 0:
             obj.draw()
     pygame.display.flip()
-    screen.fill("Black")
--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import time
 import pygame
-from pacman import Pacman
+
 from func import update_screen
+from pacman import Pacman
 
 size = (600, 700)
+clock = pygame.time.Clock()
 
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode(size)
     pacman = Pacman(size[0] / 2 - 75, size[1] / 2 + 100, screen)
     objects = [pacman]  # массив объектов
     game_over = False
+
     pygame.mixer.init()
+
     pacman.sounds.append(pygame.mixer.Sound("sounds/moving_sounds.mp3"))
     pacman.sounds.append(pygame.mixer.Sound("sounds/death_sound.mp3"))
+
     while not game_over:
         for event in pygame.event.get():
             game_over = event.type == pygame.QUIT
@@ -25,7 +30,7 @@ def main():
         update_screen(objects, screen)
         if objects[0].game_over:
             objects[0].death_animation(objects)
-
+        clock.tick()
 
 if __name__ == "__main__":
     main()
